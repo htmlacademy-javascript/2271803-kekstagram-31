4accd111ef91dbcdead75c5054438b4c4be05eef
Alena Chernova <alenvlasovav@gmail.com>
2024-03-14 00:52:41 +0300

    Добавлены некоторые переменные

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const numObject = 25;
+const minLikes = 15;
+const maxLikes = 200;
+const description = ['Красиво!', 'Однозначно лайк', 'Что это было?'];
+const message = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.'
+];
+const name = ['Артем', 'Вика', 'Мария'];
